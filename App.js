7dfa3e7eddc87ed7c7f00073a87736879f872717
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-28 19:41:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+// import logo from './logo.sv
+import React  from 'react';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+
+
+  export default function App() {
+  
+    const [simple, setsimple] = useState(" ");
+  
+    const [name, setname] = useState();
+  
+    const [fullname,setfullname]=useState();
+  
+    const [lastname,setlastname]=useState();
+  
+    const onsubmit = () => {
+      setname(simple);
+      setlastname(fullname);
+    };
+
+    function e(event) {
+      setsimple(event.target.value);
+      
+    };
+    
+    function input(event){
+      setfullname(event.target.value);
+    };
+  
+  
+    return (
+      <div className="App">
+     
+        <h1>{name}{lastname}</h1>
+        <input type="text" onChange={e} value={simple}></input>
+        <br/>
+        <input type="text" onChange={input} value={fullname}></input>
+  
+        <button onClick={onsubmit}>click me</button>
+      </div>
+    );
+  }
+  
+
